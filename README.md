fbf9a45a5f645d0ebccffea292aab6763d0f2c35
Gurdev S. Panue <gpanue@hotmail.com>
2018-10-29 21:23:11 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center">Diamond Player</h1>
+
+<div align="center">
+  A customisable HTML5 video player component for React
+</div>
+
+## Development
+
+1. Clone the repository: ``git clone https://github.com/GSPanue/diamond-player.git``
+2. Install the dependencies: ``npm install``
+1. Initialise the local server: ``npm run start``
+2. Go to: http://localhost:8080
+
+## Commands
+
+- Generate the production build: ``npm run build``
+- Open the bundle analyser: ``npm run report``
+- Run all unit tests: ``npm run test``
+- Run tests related to changed files: ``npm run test:watch``
+
+## Version
+
+0.0.0
+
+## Author
+
+Gurdev S. Panue
+
+## License
+
+Diamond Player is licensed under the MIT License.
---
+++ config/webpack.dev.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const path = require('path');
+
+const root = path.resolve(__dirname, '..');
+
+module.exports = {
+  entry: path.join(root, 'src'),
+  output: {
+    path: path.join(root, 'demo/js'),
+    publicPath: '/js/',
+    filename: 'bundle.min.js'
+  },
+  devServer: {
+    contentBase: path.join(root, 'demo'),
+    compress: true
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: '/node_modules/',
+      loader: [
+        'babel-loader',
+        'eslint-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"'
+      }
+    }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'disable',
+      generateStatsFile: true,
+      statsOptions: {
+        source: false
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+};
