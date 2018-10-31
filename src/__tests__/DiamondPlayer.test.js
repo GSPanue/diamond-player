2b0f236a785d7426646e0648afa4eab1aef546bc
Gurdev S. Panue <gpanue@hotmail.com>
2018-10-31 03:41:47 +0000

    Added more DiamondPlayer tests

--- src/__tests__/DiamondPlayer.test.js
+++ src/__tests__/DiamondPlayer.test.js
@@ -2,11 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 import DiamondPlayer from '../DiamondPlayer';
+import { Wrapper, Video } from '../styles';
 
 describe('Component: DiamondPlayer', () => {
   it('should render without crashing', () => {
     const wrapper = shallow(<DiamondPlayer />);
 
     expect(wrapper).toHaveLength(1);
+  });
+
+  it('should render a Wrapper component', () => {
+    const wrapper = shallow(<DiamondPlayer />);
+
+    expect(wrapper.find(<Wrapper />)).toHaveLength(1);
+  });
+
+  it('should render a Video component', () => {
+    const wrapper = shallow(<DiamondPlayer />);
+
+    expect(wrapper.find(<Video />)).toHaveLength(1);
   });
 });
