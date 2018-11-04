5fe68d4717fcdbb51aa6c4108c06057814859903
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-04 04:12:10 +0000

    Refactored entry component and updated tests

--- src/DiamondPlayer.js
+++ src/DiamondPlayer.js
@@ -1,44 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Wrapper, Video } from './styles';
+import { Wrapper } from './styles';
+import Video from './components/Video';
 
-const DiamondPlayer = ({ width, height, background, src, ...rest }) => (
-  <Wrapper maxWidth={width} maxHeight={height} background={background}>
-    <Video {...rest}>
-      <source src={src} />
-    </Video>
+const DiamondPlayer = ({ width, height, style, ...rest }) => (
+  <Wrapper maxWidth={width} maxHeight={height} style={style}>
+    <Video {...rest} />
   </Wrapper>
 );
 
 DiamondPlayer.propTypes = {
-  autoPlay: PropTypes.bool,
-  background: PropTypes.string,
-  controls: PropTypes.bool,
   height: PropTypes.string,
-  loop: PropTypes.bool,
-  muted: PropTypes.bool,
-  poster: PropTypes.string,
-  preload: PropTypes.oneOf([
-    'auto',
-    'metadata',
-    'none'
-  ]),
-  src: PropTypes.string,
+  source: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.array
+  ]).isRequired,
   style: PropTypes.object,
   width: PropTypes.string
 };
 
 DiamondPlayer.defaultProps = {
-  autoPlay: false,
-  background: 'transparent',
-  controls: false,
   height: '100%',
-  loop: false,
-  muted: false,
-  poster: undefined,
-  preload: undefined,
-  src: undefined,
   style: undefined,
   width: '100%'
 };
