dfeee44de408f8c204c262ab8bfcf6c3ff9ebb05
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-05 18:17:54 +0000

    Added wrapper styles

--- src/components/ControlBar/styles.js
+++ src/components/ControlBar/styles.js
@@ -1,6 +1,13 @@
 import styled from 'styled-components';
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  flex-wrap: wrap;
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,.7));
+`;
 
 export {
   Wrapper
