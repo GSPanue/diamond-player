8e87517ccc373b84ee5a60546a6ca5e7857dc705
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-06 20:29:58 +0000

    Imported Settings, Minimize, and Maximize components and updated tests

--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 
 import { Wrapper } from './styles';
-import { Play, Pause } from '../ControlButton';
+import { Play, Pause, Settings, Minimize, Maximize } from '../ControlButton';
 
 class ControlBar extends Component {
   constructor() {
     super();
 
     this.state = {
-      showPlayButton: true
+      showPlayButton: true,
+      showMaximizeButton: true
     };
   }
 
   render() {
-    const { showPlayButton } = this.state;
+    const { showPlayButton, showMaximizeButton } = this.state;
 
     return (
       <Wrapper>
         {(showPlayButton) ? <Play /> : <Pause />}
+        <Settings />
+        {(showMaximizeButton) ? <Maximize /> : <Minimize />}
       </Wrapper>
     );
   }
--- src/components/ControlBar/__tests__/ControlBar.test.js
+++ src/components/ControlBar/__tests__/ControlBar.test.js
@@ -23,11 +23,31 @@ describe('Component: ControlBar', () => {
     expect(wrapper.find('Play')).toHaveLength(1);
   });
 
-  it('should render a Pause component', () => {
+  it('should render a Pause component when showPlayButton is false', () => {
     const wrapper = shallow(<ControlBar />);
 
     expect(wrapper.find('Pause')).toHaveLength(0);
     wrapper.setState({ showPlayButton: false });
     expect(wrapper.find('Pause')).toHaveLength(1);
+  });
+
+  it('should render a Settings component', () => {
+    const wrapper = shallow(<ControlBar />);
+
+    expect(wrapper.find('Settings')).toHaveLength(1);
+  });
+
+  it('should render a Maximize component', () => {
+    const wrapper = shallow(<ControlBar />);
+
+    expect(wrapper.find('Maximize')).toHaveLength(1);
+  });
+
+  it('should render a Minimize component when showMaximizeButton is false', () => {
+    const wrapper = shallow(<ControlBar />);
+
+    expect(wrapper.find('Minimize')).toHaveLength(0);
+    wrapper.setState({ showMaximizeButton: false });
+    expect(wrapper.find('Minimize')).toHaveLength(1);
   });
 });
