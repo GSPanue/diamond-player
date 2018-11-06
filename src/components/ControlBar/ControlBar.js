7c04d2a6b87d4544e1babd8170f371195a72a798
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-06 19:05:38 +0000

    Added state to ControlBar component

--- src/components/ControlBar/ControlBar.js
+++ src/components/ControlBar/ControlBar.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { Wrapper } from './styles';
 import { Play, Pause } from '../ControlButton';
 
-const ControlBar = () => (
-  <Wrapper>
-    <Play />
-    <Pause />
-  </Wrapper>
-);
+class ControlBar extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      showPlayButton: true
+    };
+  }
+
+  render() {
+    const { showPlayButton } = this.state;
+
+    return (
+      <Wrapper>
+        {(showPlayButton) ? <Play /> : <Pause />}
+      </Wrapper>
+    );
+  }
+}
 
 export default ControlBar;
