d52a9876afc4f69d6277e453b68368c6493da18e
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-07 04:55:31 +0000

    Refactored HOC to retain component name

--- src/components/withContext/withContext.js
+++ src/components/withContext/withContext.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Context } from '../Provider';
 
-const withContext = (Component) => (
-  (props) => (
+const withContext = (Component) => {
+  const WithContext = (props) => (
     <Context.Consumer>
       {(context) => <Component {...props} context={{ ...context }} />}
     </Context.Consumer>
-  )
-);
+  );
+
+  WithContext.displayName = `withContext(${Component.displayName || Component.name})`;
+
+  return WithContext;
+};
 
 export default withContext;
