1be72803bd20047272bd0aa04adb610e03d14ed5
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-13 11:18:09 +0000

    Imported ErrorBoundary component and refactored PropTypes

--- src/DiamondPlayer.js
+++ src/DiamondPlayer.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import ErrorBoundary from './components/ErrorBoundary';
 import Provider from './components/Provider';
 import { Wrapper, InnerWrapper } from './styles';
 import Video from './components/Video';
 import ControlBar from './components/ControlBar';
 
 const DiamondPlayer = ({ style, ...rest }) => (
-  <Provider>
-    <Wrapper style={style}>
-      <InnerWrapper>
-        <Video {...rest} />
-        <ControlBar />
-      </InnerWrapper>
-    </Wrapper>
-  </Provider>
+  <ErrorBoundary>
+    <Provider>
+      <Wrapper style={style}>
+        <InnerWrapper>
+          <Video {...rest} />
+          <ControlBar />
+        </InnerWrapper>
+      </Wrapper>
+    </Provider>
+  </ErrorBoundary>
 );
 
 DiamondPlayer.propTypes = {
   source: PropTypes.oneOfType([
     PropTypes.string,
-    PropTypes.array
-  ]),
+    PropTypes.arrayOf(PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      source: PropTypes.string.isRequired,
+      hd: PropTypes.bool.isRequired
+    }))
+  ]).isRequired,
   style: PropTypes.object
 };
 
 DiamondPlayer.defaultProps = {
-  source: '',
   style: undefined
 };
 
