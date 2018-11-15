153e7f16b3a109c35f6c554f4535cc50067eb846
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-15 12:36:53 +0000

    Updated tests

--- src/demo/__tests__/App.test.js
+++ src/demo/__tests__/App.test.js
@@ -10,6 +10,18 @@ describe('Component: App', () => {
     expect(wrapper).toHaveLength(1);
   });
 
+  it('should render a Fragment component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('Fragment')).toHaveLength(1);
+  });
+
+  it('should render a GlobalStyleComponent component', () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find('GlobalStyleComponent')).toHaveLength(1);
+  });
+
   it('should render a DiamondPlayer component', () => {
     const wrapper = shallow(<App />);
 
