51d5f64aa9c0186358949d5f709805290b5b42c1
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-16 07:00:41 +0000

    Added tests for props

--- src/lib/Source/__tests__/Source.test.js
+++ src/lib/Source/__tests__/Source.test.js
@@ -4,15 +4,33 @@ import { shallow } from 'enzyme';
 import { Source } from '..';
 
 describe('Component: Source', () => {
+  const minProps = {
+    src: '',
+    srcSet: '',
+    media: '',
+    sizes: '',
+    type: ''
+  };
+
   it('should render without crashing', () => {
-    const wrapper = shallow(<Source />);
+    const wrapper = shallow(<Source {...minProps} />);
 
     expect(wrapper).toHaveLength(1);
   });
 
   it('should render a source tag', () => {
-    const wrapper = shallow(<Source />);
+    const wrapper = shallow(<Source {...minProps} />);
 
     expect(wrapper.find('source')).toHaveLength(1);
+  });
+
+  it('should have props for src, srcSet, media, sizes, and type', () => {
+    const wrapper = shallow(<Source {...minProps} />);
+
+    expect(wrapper.props().src).toBeDefined();
+    expect(wrapper.props().srcSet).toBeDefined();
+    expect(wrapper.props().media).toBeDefined();
+    expect(wrapper.props().sizes).toBeDefined();
+    expect(wrapper.props().type).toBeDefined();
   });
 });
