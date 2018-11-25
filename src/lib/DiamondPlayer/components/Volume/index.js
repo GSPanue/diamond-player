c85bd3d4f212a53278d34b1c53130d5a7bb77ce3
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-25 16:55:39 +0000

    Connected Volume to Redux store

--- src/lib/DiamondPlayer/components/Volume/index.js
+++ src/lib/DiamondPlayer/components/Volume/index.js
@@ -1,1 +1,14 @@
-export { default } from './Volume';
+import { connect } from 'react-redux';
+
+import { adjustVolume } from '../../actions';
+import Volume from './Volume';
+
+const mapStateToProps = (state) => ({
+  value: state.video.volume
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  adjustVolume: (value) => dispatch(adjustVolume(value))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Volume);
