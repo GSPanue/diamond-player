24abd19d17a1fbe124f71652ead5889a2b013dc4
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-26 20:11:21 +0000

    Added Time component

---
+++ src/lib/DiamondPlayer/components/Time/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  margin-left: 8px;
+  align-self: center;
+  font-size: 13px;
+  text-shadow: 0 1px 1px rgba(0,0,0,.15);
+`;
+
+export {
+  Wrapper
+};
---
+++ src/lib/DiamondPlayer/components/Time/types.js
@@ -0,0 +1,10 @@
+import PropTypes from 'prop-types';
+
+const propTypes = {
+  currentTime: PropTypes.string.isRequired,
+  duration: PropTypes.string.isRequired
+};
+
+export {
+  propTypes
+};
