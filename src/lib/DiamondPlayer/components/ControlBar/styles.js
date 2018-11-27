425329aba8af31d2630dde847042907ef20e0ec1
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-27 19:14:12 +0000

    Refactored styles

--- src/lib/DiamondPlayer/components/ControlBar/styles.js
+++ src/lib/DiamondPlayer/components/ControlBar/styles.js
@@ -1,23 +1,13 @@
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  max-width: 100%;
-  position: absolute;
   display: flex;
-  flex-direction: row;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  flex-wrap: wrap;
-  padding: 35px 15px 10px;
-  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,.7));
-  user-select: none;
+  justify-content: space-between;
+  padding-top: 6px;
 `;
 
 const Section = styled.div`
-  width: 50%;
   display: flex;
-  justify-content: ${({ align }) => ((align === 'left') ? 'flex-start' : 'flex-end')};
 `;
 
 const Group = styled.div`
