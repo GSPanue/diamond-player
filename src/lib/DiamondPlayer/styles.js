3cdcf4cc07342fefc6d1a23ade4148741956bc61
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-27 17:43:33 +0000

    Added inner wrapper component

--- src/lib/DiamondPlayer/styles.js
+++ src/lib/DiamondPlayer/styles.js
@@ -9,6 +9,21 @@ const Wrapper = styled.div`
   background: #000000;
 `;
 
+const InnerWrapper = styled.div`
+  max-width: 100%;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  padding: 35px 15px 10px;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  user-select: none;
+  background: linear-gradient(rgba(0,0,0,0),rgba(0,0,0,.7));
+`;
+
 export {
-  Wrapper
+  Wrapper,
+  InnerWrapper
 };
