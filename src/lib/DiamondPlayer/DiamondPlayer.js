feee19cb069dfce49a27b9e88b3aa15f4bf15bbb
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-27 19:13:05 +0000

    Wrapped ProgressBar and ControlBar

--- src/lib/DiamondPlayer/DiamondPlayer.js
+++ src/lib/DiamondPlayer/DiamondPlayer.js
@@ -4,15 +4,19 @@ import { Provider } from 'react-redux';
 import store from './store';
 
 import { propTypes, defaultProps } from './types';
-import { Wrapper } from './styles';
+import { Wrapper, InnerWrapper } from './styles';
 import Video from './components/Video';
 import ControlBar from './components/ControlBar';
+import ProgressBar from './components/ProgressBar';
 
 const DiamondPlayer = ({ style, ...rest }) => (
   <Provider store={store}>
     <Wrapper style={style}>
       <Video {...rest} />
-      <ControlBar />
+      <InnerWrapper>
+        <ProgressBar />
+        <ControlBar />
+      </InnerWrapper>
     </Wrapper>
   </Provider>
 );
