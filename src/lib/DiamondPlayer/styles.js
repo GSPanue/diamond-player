9601c4554daafbfd5bbb450d3c0983b8e57ecc36
Gurdev S. Panue <gpanue@hotmail.com>
2018-11-28 21:48:34 +0000

    Increased padding

--- src/lib/DiamondPlayer/styles.js
+++ src/lib/DiamondPlayer/styles.js
@@ -16,7 +16,7 @@ const InnerWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  padding: 35px 15px 6px;
+  padding: 35px 15px 10px;
   left: 0;
   right: 0;
   bottom: 0;
