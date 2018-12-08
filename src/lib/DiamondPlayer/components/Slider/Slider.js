5a834b314bfe8870420c0529e9ad1e3210589362
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-08 23:08:52 +0000

    Changed range slider implementation

--- src/lib/DiamondPlayer/components/Slider/Slider.js
+++ src/lib/DiamondPlayer/components/Slider/Slider.js
@@ -1,40 +1,137 @@
 import React, { PureComponent } from 'react';
 
+import { getMousePosition, getHorizontalBoundaries } from '../../helpers';
+
 import { propTypes, defaultProps } from './types';
-import { Wrapper, Input, Trail } from './styles';
+import { Wrapper, InnerWrapper, Bar, Trail, Thumb } from './styles';
 
 class Slider extends PureComponent {
   constructor() {
     super();
 
-    this.handleChange = this.handleChange.bind(this);
+    this.wrapper = React.createRef();
+    this.thumb = React.createRef();
+
+    this.handleResize = this.handleResize.bind(this);
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.handleMouseUp = this.handleMouseUp.bind(this);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+
+    this.state = {
+      mouseDown: false,
+      boundary: {
+        left: 0,
+        right: 100
+      }
+    };
+  }
+
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
   }
 
   /**
-   * handleChange: Handles a change event.
+   * handleResize: Handles a resize event.
    */
-  handleChange({ target }) {
-    const { value } = target;
-    const { onChange: callback } = this.props;
+  handleResize() {
+    const { current: wrapper } = this.wrapper;
+    const { current: thumb } = this.thumb;
 
-    callback(value);
+    // Get wrapper boundaries
+    const boundary = getHorizontalBoundaries(wrapper, thumb);
+
+    this.setState({
+      boundary
+    });
+  }
+
+  /**
+   * handleMouseDown: Handles a mousedown event.
+   */
+  handleMouseDown({ pageX }) {
+    window.addEventListener('mousemove', this.handleMouseMove);
+    window.addEventListener('mouseup', this.handleMouseUp);
+
+    this.handleChange(pageX);
+    this.setState({ mouseDown: true });
+  }
+
+  /**
+   * handleMouseUp: Handles a mouseup event.
+   */
+  handleMouseUp() {
+    window.removeEventListener('mousemove', this.handleMouseMove);
+    window.removeEventListener('mouseup', this.handleMouseUp);
+
+    this.setState({ mouseDown: false });
+  }
+
+  /**
+   * handleMouseMove: Handles a mousemove event.
+   */
+  handleMouseMove({ pageX }) {
+    const { mouseDown } = this.state;
+
+    if (mouseDown) {
+      this.handleChange(pageX);
+    }
+  }
+
+  /**
+   * handleChange: Handles a value change.
+   */
+  handleChange(pageX) {
+    const { value: currentValue, onChange: callback } = this.props;
+    const nextValue = this.positionToValue(pageX);
+
+    const hasChangedValue = (currentValue !== nextValue);
+
+    if (hasChangedValue) {
+      callback(nextValue);
+    }
+  }
+
+  /**
+   * positionToValue: Converts mouse position to a slider value.
+   */
+  positionToValue(pageX) {
+    const { current: wrapper } = this.wrapper;
+    const { boundary } = this.state;
+
+    // Get mouse position relative to the wrapper in percentage
+    const position = getMousePosition(wrapper, boundary, pageX);
+
+    // Subtract left from position and right boundary
+    // This gives a range of 0 - 80
+    const x1 = (position - boundary.left);
+    const x2 = (boundary.right - boundary.left);
+
+    // Return a slider value between 0.00 and 1.00
+    return Math.round((x1 / x2) * 1e2) / 1e2;
   }
 
   render() {
-    const { value, ...rest } = this.props;
+    const { value, innerWrapperHeight, barHeight, showThumb, ...rest } = this.props;
+    const { boundary } = this.state;
 
     return (
-      <Wrapper>
-        <Input
-          type="range"
-          min="0"
-          max="1"
-          step="0.01"
-          value={value}
-          {...rest}
-          onChange={this.handleChange}
-        />
-        <Trail trail={value} />
+      <Wrapper
+        {...rest}
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        ref={this.wrapper}
+      >
+        <InnerWrapper innerWrapperHeight={innerWrapperHeight}>
+          <Bar barHeight={barHeight} />
+          <Trail position={value} trailHeight={barHeight} boundary={boundary}>
+            <Thumb position={value} showThumb={showThumb} ref={this.thumb} />
+          </Trail>
+        </InnerWrapper>
       </Wrapper>
     );
   }
--- src/lib/DiamondPlayer/components/Slider/styles.js
+++ src/lib/DiamondPlayer/components/Slider/styles.js
@@ -3,43 +3,57 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  position: relative;
-  display: flex;
+  padding: 7px 0;
+  cursor: pointer;
 `;
 
-const Input = styled.input`
-  appearance: none;
+const InnerWrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: ${({ innerWrapperHeight }) => (innerWrapperHeight || '100%')};
+  position: relative;
+  display: flex;
+  align-items: center;
+`;
+
+const Bar = styled.div`
+  width: 100%;
+  height: ${({ barHeight }) => (barHeight || '3px')};
   padding: 0;
   margin: 0;
   outline: none;
   z-index: 3;
   background: rgba(255,255,255,.10);
-  cursor: pointer;
-  
-  &::-webkit-slider-thumb {
-    appearance: none;
-    width: 11px;
-    height: 11px;
-    border: 0;
-    border-radius: 100%;
-    box-shadow: 0 1px 1px rgba(0,0,0,.15), 0 0 0 1px rgba(47,52,61,.2);
-    background: #FFFFFF;
-    opacity: ${({ showThumb }) => +showThumb};
-    transition: opacity 0.2s 0.1s ease;
-  }
 `;
 
-const Trail = styled.div`
-  width: ${({ trail }) => trail * 100}%;
-  height: 100%;
+const Trail = styled.div.attrs(({ position, boundary }) => ({
+  style: {
+    width: `${(position * (boundary.right - boundary.left)) + boundary.left}%`
+  }
+}))`
+  height: ${({ trailHeight }) => (trailHeight || '3px')};
   position: absolute;
+  display: flex;
+  align-items: center;
   background: #FFFFFF;
+`;
+
+const Thumb = styled.div`
+  right: -5px;
+  height: 11px;
+  width: 11px;
+  position: absolute;
+  border: 0;
+  border-radius: 100%;
+  box-shadow: 0 1px 1px rgba(0,0,0,.15), 0 0 0 1px rgba(47,52,61,.2);
+  background: #FFFFFF;
+  opacity: ${({ showThumb }) => +showThumb};
+  transition: opacity 0.2s 0.1s ease;
 `;
 
 export {
   Wrapper,
-  Input,
-  Trail
+  InnerWrapper,
+  Bar,
+  Trail,
+  Thumb
 };
