3ee940b1d106bd58ab6753167eef5a9c7fe02283
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-08 23:09:50 +0000

    Updated Slider PropTypes

--- src/lib/DiamondPlayer/components/Slider/types.js
+++ src/lib/DiamondPlayer/components/Slider/types.js
@@ -1,14 +1,20 @@
 import PropTypes from 'prop-types';
 
 const propTypes = {
-  value: PropTypes.string,
+  value: PropTypes.number,
   showThumb: PropTypes.bool,
+  innerWrapperHeight: PropTypes.string,
+  barHeight: PropTypes.string,
+  className: PropTypes.string,
   onChange: PropTypes.func.isRequired
 };
 
 const defaultProps = {
-  value: '0',
-  showThumb: true
+  value: 0,
+  showThumb: true,
+  innerWrapperHeight: undefined,
+  barHeight: undefined,
+  className: undefined
 };
 
 export {
