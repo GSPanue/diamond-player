6ddeacbb155bca08721ddfe34494550a74ce82c6
Gurdev S. Panue <gpanue@hotmail.com>
2018-12-10 23:32:57 +0000

    Added type check helper for type undefined

--- src/lib/DiamondPlayer/helpers/type/__tests__/type.test.js
+++ src/lib/DiamondPlayer/helpers/type/__tests__/type.test.js
@@ -1,6 +1,21 @@
-import { isFunction } from '..';
+import { isUndefined, isFunction } from '..';
 
 describe('Helper: Type', () => {
+  describe('Function: isUndefined', () => {
+    it('should return true when passed undefined', () => {
+      expect(isUndefined(undefined)).toBeTrue();
+    });
+
+    it('should return false when not passed undefined', () => {
+      expect(isUndefined(null)).toBeFalse();
+      expect(isUndefined(false)).toBeFalse();
+      expect(isUndefined(0)).toBeFalse();
+      expect(isUndefined('')).toBeFalse();
+      expect(isUndefined({})).toBeFalse();
+      expect(isUndefined(() => {})).toBeFalse();
+    });
+  });
+
   describe('Function: isFunction', () => {
     it('should return true when passed a function', () => {
       expect(isFunction(() => {})).toBeTrue();
@@ -8,7 +23,9 @@ describe('Helper: Type', () => {
 
     it('should return false when not passed a function', () => {
       expect(isFunction(undefined)).toBeFalse();
+      expect(isFunction(null)).toBeFalse();
       expect(isFunction(false)).toBeFalse();
+      expect(isFunction(0)).toBeFalse();
       expect(isFunction('')).toBeFalse();
       expect(isFunction({})).toBeFalse();
     });
--- src/lib/DiamondPlayer/helpers/type/type.js
+++ src/lib/DiamondPlayer/helpers/type/type.js
@@ -1,7 +1,12 @@
+const isUndefined = (value) => (
+  typeof value === 'undefined'
+);
+
 const isFunction = (value) => (
   typeof value === 'function'
 );
 
 export {
+  isUndefined,
   isFunction
 };
