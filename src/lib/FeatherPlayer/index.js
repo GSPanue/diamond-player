5e9fddf79b519b5a7788e496de3d6129cf0943b1
Gurdev S. Panue <gpanue@hotmail.com>
2019-04-19 18:14:18 +0100

    Moved FeatherPlayer src up one level

--- src/lib/FeatherPlayer/index.js
+++
@@ -1,7 +0,0 @@
-import { memo } from 'react';
-
-const FeatherPlayer = memo(require('./FeatherPlayer').default);
-
-FeatherPlayer.displayName = 'FeatherPlayer';
-
-export default FeatherPlayer;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,7 @@
-export { default } from './FeatherPlayer';
+import { memo } from 'react';
+
+const FeatherPlayer = memo(require('./FeatherPlayer').default);
+
+FeatherPlayer.displayName = 'FeatherPlayer';
+
+export default FeatherPlayer;
